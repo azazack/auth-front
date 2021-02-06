d8160041195ddebbb3984b42b2b71faa4ec58b10
azazack <zakaria.azf@pre-history.com>
2021-02-06 17:37:35 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .container
+    .home
+      Logo
+      h1.title auth-front
+      .links
+        a.button--green(href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer") Documentation
+        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer") GitHub
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+  .links {
+    padding-top: 15px;
+  }
+}
+</style>
